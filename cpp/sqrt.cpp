efb28123d17597e789b073ba3fbb4f16f4535a8e
Tirth Patel <t83patel@uwaterloo.ca>
2024-08-29 13:20:12 -0400

    august 29th ones

---
+++ cpp/sqrt.cpp
@@ -0,0 +1,24 @@
+//august 29 2024
+// beats 100.00% of cpp submissions
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) {
+            return x;
+        }
+        int first = 1;
+        int last = x;
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+            // for some reason i get an error doing mid * mid == x
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid > x / mid) {
+                last = mid - 1;
+            } else {
+                first = mid + 1;
+            }
+        }
+        return last;
+    }
+};
---
+++ python/plusOne.py
@@ -0,0 +1,14 @@
+#august 29 2024
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in reversed(range(len(digits))):
+            if digits[i] != 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
