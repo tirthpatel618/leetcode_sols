6507276895438a6eeef10c90d0e8e7bac63e0aa6
Tirth Patel <t83patel@uwaterloo.ca>
2025-08-06 14:10:56 -0400

    2 from hashing and arrays

---
+++ python/groupAnagrams.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+def groupAnagrams(strs):
+    res = defaultdict(list)
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        res[tuple(count)].append(s)
+    return list(res.values())
+
