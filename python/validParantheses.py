8c63b6dec83cdc07914e3278c397b5687ed5defe
Tirth Patel <t83patel@uwaterloo.ca>
2025-08-11 20:08:37 -0400

    finish stack and starting binary search

--- python/validParantheses.py
+++ python/validParantheses.py
@@ -9,4 +9,21 @@ class Solution:
                 stack.append(i)
             elif len(stack) == 0 or brackets[stack.pop()] != i:
                 return False
-        return len(stack) == 0        
+        return len(stack) == 0    
+
+# or 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        brackets = {"]":"[", ")":"(", "}":"{"}
+        for b in s:
+            if b in brackets:
+                if stack and stack[-1] == brackets[b]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(b)
+        return True if not stack else False
+        
+            
